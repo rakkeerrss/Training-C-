3576f60ca7ead6e0f5f4b78e101fe4d7d4c234c0
Aleksandr Khomenko <rakkerrss@gmail.com>
2022-06-12 00:07:55 +1000

    main

--- lesson_two/ExerciseOne/EnterNumber.cpp
+++ lesson_two/ExerciseOne/EnterNumber.cpp
@@ -2,28 +2,28 @@
 
 int prover (double oneNumber, double twoNumber, char oper)
 {
-    if (oper == 0)
+    if (oper == '+')
     {
         double zero(oneNumber + twoNumber);
         std::cout << oneNumber << " + " << twoNumber 
         << " = " << zero;
     }
 
-    if (oper == 1)
+    if (oper == '-')
     {
         double one(oneNumber - twoNumber);
         std::cout << oneNumber << " - " << twoNumber 
         << " = " << one;
     }
 
-    if (oper == 2)
+    if (oper == '*')
     {
         double two(oneNumber * twoNumber);
         std::cout << oneNumber << " * " << twoNumber 
         << " = " << two;
     }
 
-    if (oper == 3)
+    if (oper == '/')
     {
         double del(oneNumber / twoNumber);
         std::cout << oneNumber << " / " << twoNumber 
@@ -34,15 +34,15 @@ int prover (double oneNumber, double twoNumber, char oper)
 }
 
 
-int EnterNumber (double oneNumber, double twoNumber, int oper)
+int EnterNumber (double oneNumber, double twoNumber, char oper)
 {
     std::cout << "Enter a double value: ";
     std::cin >> oneNumber;
     std::cout << "Enter a double value: ";
     std::cin >> twoNumber;
-    std::cout << "Enter one of the following: 0 or +, 1 or -, 2 or *5, 3 or /: ";
+    std::cout << "Enter one of the following: +, -, *, /: ";
     std::cin >> oper;
-    if (oper <= 3)
+    if (oper == '+' || oper == '-' || oper == '*' || oper == '/')
         prover(oneNumber, twoNumber, oper);
     else
         std::cout << "You entered an invalid character:  " << oper;
